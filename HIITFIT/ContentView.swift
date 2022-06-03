a01be0ff7c8a2ff258065f9dccafabe8e3fbecb0
YJ.Li <yuanjun.li@beyond-power.com>
2022-06-03 21:57:47 +0800

    Initial Commit

---
+++ HIITFIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HIITFIT
+//
+//  Created by YJ.Lee on 2022/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HIITFIT/HIITFITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFITApp.swift
+//  HIITFIT
+//
+//  Created by YJ.Lee on 2022/6/3.
+//
+
+import SwiftUI
+
+@main
+struct HIITFITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
